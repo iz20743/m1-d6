f9011b801f055925979982c83328e043a05e2449
iz20743 <93397767+iz20743@users.noreply.github.com>
2022-07-04 13:59:01 +0100

    full-name-and-birthyear

---
+++ index.js
@@ -0,0 +1,17 @@
+
+// going  to print my name using the veribales
+
+let firstName = "ibrahim";   // creating the veriable for first name
+let lastName = "zulfiqar";   // creating the veriable for last name
+let fullName = firstName + lastName;   // creating the veriable to combine fist and second name in fullName
+
+
+console.log("my full name is ", fullName);   // Printing my fullName
+
+let age = 25;                      // creating the veriable for my age
+let currentYear = 2022;           // creating the veriable for current year
+let myBirthYear = currentYear - age;    // creating the veriable for to evalute my birthyear
+
+console.log("the year i was born in", myBirthYear);        // printing the year i was boen in
+
+
